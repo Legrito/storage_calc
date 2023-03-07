561122f271a716ace06c9b7fb240d3cce1dbe66b
Lesya <jamma@i.ua>
2023-03-07 16:30:42 +0100

    Update UI

--- js/calculator.js
+++ js/calculator.js
@@ -9,19 +9,11 @@
       minValue: 0,
       maxValue: 10,
       storagePrice: 0.01,
-      // storagePrice: {
-      //   hdd: 0.01,
-      //   ssd: 0.02,
-      // },
       transferPrice: 0.01,
     },
     scaleway: {
       minValue: 0,
-      storagePrice: 0.06,
-      // storagePrice: {
-      //   multi: 0.06, // 75 gb free
-      //   single: 0.03, // 75gb free
-      // },
+      storagePrice: 0.03, // 75gb for free
       transferPrice: 0.02, //75gb free
     },
     vultr: {
@@ -31,32 +23,54 @@
     },
   };
 
-  const containerEl = document.querySelector(".container");
-
   const storageInput = document.getElementById("storage");
   const storageLabel = document.getElementById("storage_label");
   const transferInput = document.getElementById("transfer");
   const transferLabel = document.getElementById("transfer_label");
 
   const servicesEl = document.querySelectorAll(".services > div");
+  const formsEl = document.querySelectorAll(".storage-form");
 
   let storageAmount = 0;
   let transferAmount = 0;
 
-  const getAmount = ({ minValue, storagePrice, transferPrice }) => {
+  const getAmount = ({ minValue, storagePrice, transferPrice, maxValue }) => {
     const amount =
       storageAmount * storagePrice + transferAmount * transferPrice;
 
-    return amount >= minValue ? amount : minValue;
+    if (maxValue) {
+      return amount < minValue
+        ? minValue
+        : amount > maxValue
+        ? maxValue
+        : amount;
+    } else {
+      return amount < minValue ? minValue : amount;
+    }
   };
 
-  const getHeight = ({ minValue, storagePrice, transferPrice, maxValue }) => {
+  const getHeight = ({
+    minValue,
+    storagePrice,
+    transferPrice,
+    maxValue,
+    key,
+  }) => {
+    if (key === "scaleway" && storageAmount <= 75 && transferAmount <= 75) {
+      return 0;
+    }
     const step = maxValue
       ? maxValue / 100
       : (1000 * storagePrice + 1000 * transferPrice) / 100;
-    const amount = getAmount({ minValue, storagePrice, transferPrice });
-
-    return amount / step;
+    let amount;
+    if (key === "scaleway" && storageAmount <= 75) {
+      amount = getAmount({ minValue, storagePrice: 0, transferPrice });
+    } else if (key === "scaleway" && transferAmount <= 75) {
+      amount = getAmount({ minValue, storagePrice, transferPrice: 0 });
+    } else {
+      amount = getAmount({ minValue, storagePrice, transferPrice, maxValue });
+    }
+    return amount > maxValue ? maxValue / step : amount / step;
   };
 
   const getLowerPriced = nodeList => {
@@ -64,30 +78,41 @@
 
     nodeList.forEach((elem, idx, arr) => {
       if (
-        +elem.getAttribute("amount").replace("$", "") <
-        +lowestPriced.getAttribute("amount").replace("$", "")
+        +elem.getAttribute("amount")?.replace("$", "") <
+        +lowestPriced.getAttribute("amount")?.replace("$", "")
       ) {
         lowestPriced = elem;
       } else {
         elem.classList.remove("lowest");
       }
     });
+
     return lowestPriced.classList.add("lowest");
+  };
+
+  const clean = node => {
+    node.removeAttribute("amount");
+    node.style.height = "0";
   };
 
   const updateUI = () => {
     Object.keys(data).forEach(key => {
       servicesEl.forEach(node => {
+        if (storageAmount === 0 && transferAmount === 0) {
+          return clean(node);
+        }
         if (node.id === key) {
-          const { minValue, storagePrice, transferPrice } = data[key];
+          const { minValue, storagePrice, transferPrice, maxValue } = data[key];
 
           node.style.height = `${getHeight({
             minValue,
             storagePrice,
             transferPrice,
+            maxValue,
+            key,
           })}%`;
           let amount = `${Math.round(
-            getAmount({ minValue, storagePrice, transferPrice })
+            getAmount({ minValue, storagePrice, transferPrice, maxValue })
           )}$`;
           node.setAttribute("amount", amount);
         }
@@ -110,6 +135,13 @@
 
     updateUI();
   };
+
+  formsEl.forEach(el =>
+    el.addEventListener("change", e => {
+      data[e.target.name].storagePrice = +e.target.value;
+      updateUI();
+    })
+  );
 
   storageInput.addEventListener("input", handleStorageChange);
 
